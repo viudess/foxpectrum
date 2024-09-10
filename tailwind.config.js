5b098df5d98ac11aabf90572fae182467f596ae3
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-10 12:34:37 -0300

    banner

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,10 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {fontFamily: {
+      'offside': ['Offside', 'sans-serif']
+    }
+  },
   },
   plugins: [],
 }
