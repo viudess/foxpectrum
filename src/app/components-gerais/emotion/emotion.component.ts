154f0a40e2c26e20e28ea6cb10b0d9fcd070dc7b
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-13 13:44:57 -0300

    forms-mood-emo

--- src/app/components-gerais/emotion/emotion.component.ts
+++ src/app/components-gerais/emotion/emotion.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-emotion',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './emotion.component.css'
 })
 export class EmotionComponent {
-
+  @Input() title!: string;
+  @Input() image!: string;
 }
--- src/app/tipico/mood/forms-mood/forms-mood.component.ts
+++ src/app/tipico/mood/forms-mood/forms-mood.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrl: './forms-mood.component.css'
 })
 export class FormsMoodComponent {
-
+  emotions = [
+    {title: 'Feliz', image: 'alegrar 1.png'},
+    {title: 'Triste', image: 'tristeza 1.png'},
+    {title: 'Irritado', image: 'irritação 1.png'},
+    {title: 'Cansado', image: 'cansaço 1.png'},
+    {title: 'Saturado', image: 'saturado 1.png'},
+    {title: 'Tranquilo', image: 'tranquilo 1.png'},
+    {title: 'Envergonhado', image: 'vergonha 1.png'},
+    {title: 'Amado', image: 'amado 1.png'},
+    {title: 'Nervoso', image: 'nervos 1.png'},
+    {title: 'Enjoado', image: 'nojo 1.png'},
+    {title: 'Medroso', image: 'Medo 1.png'},
+    {title: 'Aborrecido', image: 'aborrecer 1.png'},
+    {title: 'Orgulhoso', image: 'orgulhoso 1.png'},
+    {title: 'Preocupado', image: 'preocupado 1.png'},
+    {title: 'Ansioso', image: 'ansioso 1.png'},
+    {title: 'Curioso', image: 'curioso 1.png'},
+    {title: 'Desanimado', image: 'desanimado 1.png'},
+    {title: 'Distraído', image: 'distraído 1.png'},
+    {title: 'Confuso', image: 'confuso 1.png'},
+    {title: 'Indiferente', image: 'estou mais ou menos 1.png'},
+    {title: 'Sério', image: 'sério 1.png'},
+  ]
 }
